eb5a28fa38fbf38103de532ae61756d613a995e6
Édipo <edipo2s@gmail.com>
2014-09-12 14:11:35 -0300

    Login interface com fragment

--- app/src/main/java/com/ediposouza/ecalendar/LoginActivity.java
+++ app/src/main/java/com/ediposouza/ecalendar/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.ediposouza.ecalendar;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,9 +10,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends ActionBarActivity {
+
+    private static final String SP_NAME = "lg";
+    private static final String SP_LAST_USER = "lastUser";
+
+    //To inform which fragment is showing
+    private boolean registering;
+
+    private SharedPreferences sharedPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,7 @@ public class LoginActivity extends ActionBarActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        sharedPrefs = getSharedPreferences(SP_NAME, MODE_PRIVATE);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
@@ -26,12 +38,23 @@ public class LoginActivity extends ActionBarActivity {
         }
     }
 
+    /*
+    * Change between Login and New User Fragments
+    */
+    private void swapFragments(){
+        Fragment fragment = (registering) ? new PlaceholderFragment() : new NewUserFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+        registering = !registering;
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    class PlaceholderFragment extends Fragment {
 
-        private EditText etLogin;
+        private EditText etUser;
         private EditText etPass;
 
         public PlaceholderFragment() {
@@ -47,8 +70,108 @@ public class LoginActivity extends ActionBarActivity {
         @Override
         public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
             super.onViewCreated(view, savedInstanceState);
-            etLogin = (EditText) view.findViewById(R.id.etLogin);
+            etUser = (EditText) view.findViewById(R.id.etUser);
             etPass = (EditText) view.findViewById(R.id.etPass);
+            checkLastUser();
+            Button btLogin = (Button) view.findViewById(R.id.btLogin);
+            Button btNewUser = (Button) view.findViewById(R.id.btNewUser);
+            btLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onLoginClick();
+                }
+            });
+            btNewUser.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    swapFragments();
+                }
+            });
+        }
+
+        /*
+        * Verify and get the last user name
+        */
+        private void checkLastUser() {
+            String lastUser = sharedPrefs.getString(SP_LAST_USER, "");
+            if("".equals(lastUser))
+                etUser.requestFocus();
+            else{
+                etUser.setText(lastUser);
+                etPass.requestFocus();
+            }
+        }
+
+        private void onLoginClick() {
+            String userName = etUser.getText().toString();
+            String pass = etPass.getText().toString();
+            int spUserHash = sharedPrefs.getInt(userName, 0);
+            int userHash = userName.concat(pass).hashCode();
+            if(spUserHash != userHash){
+                etPass.setError(getString(R.string.login_error_user_pass));
+                Toast.makeText(getActivity(), getString(R.string.login_error_user_pass), Toast.LENGTH_SHORT).show();
+                return;
+            }
+            sharedPrefs.edit().putString(SP_LAST_USER, userName).commit();
+            //start home
+            Toast.makeText(getActivity(), "Login OK", Toast.LENGTH_SHORT).show();
         }
     }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    class NewUserFragment extends Fragment {
+
+        private EditText etUser;
+        private EditText etPass;
+        private EditText etPassConfirm;
+
+        public NewUserFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_new_user, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            etUser = (EditText) view.findViewById(R.id.etUser);
+            etPass = (EditText) view.findViewById(R.id.etPass);
+            etPassConfirm = (EditText) view.findViewById(R.id.etPassConfirm);
+            etUser.requestFocus();
+            Button btRegister = (Button) view.findViewById(R.id.btRegister);
+            Button btCancel = (Button) view.findViewById(R.id.btCancel);
+            btRegister.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onRegisterClick();
+                }
+            });
+            btCancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    swapFragments();
+                }
+            });
+        }
+
+        private void onRegisterClick() {
+            String pass = etPass.getText().toString();
+            if(!pass.equals(etPassConfirm.getText().toString())) {
+                etPass.setError(getString(R.string.login_error_pass_match));
+                etPassConfirm.setError(getString(R.string.login_error_pass_match));
+                etPass.requestFocus();
+                return;
+            }
+            String userKey = etUser.getText().toString();
+            int userHash = userKey.concat(pass).hashCode();
+            sharedPrefs.edit().putInt(userKey, userHash).commit();
+        }
+    }
+
 }
