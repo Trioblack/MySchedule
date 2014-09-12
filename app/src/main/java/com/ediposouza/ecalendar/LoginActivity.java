b8e6daf5cc05dfb727027af8b68ff764e77f5a21
Édipo <edipo2s@gmail.com>
2014-09-12 12:49:43 -0300

    Login interface com fragment

--- app/src/main/java/com/ediposouza/ecalendar/LoginActivity.java
+++ app/src/main/java/com/ediposouza/ecalendar/LoginActivity.java
@@ -1,15 +1,17 @@
 package com.ediposouza.ecalendar;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
 
 public class LoginActivity extends ActionBarActivity {
-
-    private EditText etLogin;
-    private EditText etPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +19,36 @@ public class LoginActivity extends ActionBarActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
-        etLogin = (EditText) findViewById(R.id.etLogin);
-        etPass = (EditText) findViewById(R.id.etPass);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        private EditText etLogin;
+        private EditText etPass;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            etLogin = (EditText) view.findViewById(R.id.etLogin);
+            etPass = (EditText) view.findViewById(R.id.etPass);
+        }
+    }
 }
